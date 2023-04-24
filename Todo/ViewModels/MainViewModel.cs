e56f9e508bcd405d7ee0929439965b7ba5ca5fc6
GByteos <gbyte@gb-tronix.de>
2023-04-24 21:47:35 +0200

    Created View model and added namnespaces to the XAML

---
+++ Todo/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Todo.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    string inputText;
+}
