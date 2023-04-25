ac0b895adcc01c9e136fe58d2e5899f766263bf3
GByteos <gbyte@gb-tronix.de>
2023-04-25 20:28:55 +0200

    Added content of detailViewModel and did the binding

--- Todo/ViewModels/MainViewModel.cs
+++ Todo/ViewModels/MainViewModel.cs
@@ -35,4 +35,10 @@ public partial class MainViewModel : ObservableObject
             Items.Remove(s);
         }
     }
+
+    [RelayCommand]
+    async Task Tap(string s)
+    {
+        await Shell.Current.GoToAsync($"{nameof(DetailPage)}?Text={s}");
+    }
 }
